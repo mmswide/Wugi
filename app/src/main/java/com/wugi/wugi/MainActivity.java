37ee9172f450c63fb2a149f0cee62276c99fd745
unknown <mobileguru121@gmail.com>
2017-11-25 03:20:14 -0800

    add fragments for navigationdrawer

--- app/src/main/java/com/wugi/wugi/MainActivity.java
+++ app/src/main/java/com/wugi/wugi/MainActivity.java
@@ -1,6 +1,9 @@
 package com.wugi.wugi;
 
+import android.net.Uri;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,7 +14,9 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.wugi.wugi.fragments.FragmentOne;
+
+public class MainActivity extends AppCompatActivity implements FragmentOne.OnFragmentInteractionListener {
 
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
@@ -22,6 +27,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        if (savedInstanceState == null) {
+            Fragment fragment = null;
+            Class fragmentClass = null;
+            fragmentClass = FragmentOne.class;
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+        }
+
         initNavigationDrawer();
     }
 
@@ -33,14 +53,19 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(MenuItem menuItem) {
 
                 int id = menuItem.getItemId();
+                Fragment fragment = null;
+                Class fragmentClass = null;
 
                 switch (id){
                     case R.id.home:
                         Toast.makeText(getApplicationContext(),"Home",Toast.LENGTH_SHORT).show();
+                        fragmentClass = FragmentOne.class;
                         drawerLayout.closeDrawers();
                         break;
                     case R.id.upcoming:
                         Toast.makeText(getApplicationContext(),"Upcoming",Toast.LENGTH_SHORT).show();
+                        fragmentClass = FragmentOne.class;
+                        drawerLayout.closeDrawers();
                         break;
                     case R.id.photos:
                         Toast.makeText(getApplicationContext(),"Photos",Toast.LENGTH_SHORT).show();
@@ -61,6 +86,15 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.logout:
                         finish();
                 }
+
+                try {
+                    fragment = (Fragment) fragmentClass.newInstance();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
                 return true;
             }
         });
@@ -83,5 +117,10 @@ public class MainActivity extends AppCompatActivity {
         };
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
