cba927d3abd2607043f7c0a169f5a24a796ff4a0
unknown <mobileguru121@gmail.com>
2018-01-27 09:21:30 -0800

    add firebase notification

---
+++ app/src/main/java/com/wugi/inc/services/WFirebaseMessagingService.java
@@ -0,0 +1,23 @@
+package com.wugi.inc.services;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by storm on 1/27/2018.
+ */
+
+public class WFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "FCM Service";
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO: Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+    }
+}
